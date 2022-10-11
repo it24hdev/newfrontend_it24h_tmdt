8ce822d64b00561043c93024566b2180a4643c2f
toanbv <sky97tb@gmail.com>
2022-10-11 18:40:08 +0700

    no message

--- app/Imports/MenuImport.php
+++ app/Imports/MenuImport.php
@@ -32,90 +32,101 @@ class MenuImport implements ToCollection, SkipsEmptyRows, WithStartRow, WithVali
 
     public function collection(Collection $rows)
     {
-      foreach ($rows as $key => $row) {
-        $exists = db::table('admin_menu_items')->where('ma',$row[1])->where('menu',$this->menu)->first();
+      foreach ($rows as $key => $col) {
+        $exists = db::table('admin_menu_items')->where('ma',$col[1])->where('menu',$this->menu)->first();
         if(!empty($exists)){
             $MenuItems =  MenuItems::find($exists->id);
-            $MenuItems->label    = $row[0];
-            if(!empty($row[3]))
-            {$MenuItems->depth    = $row[3];}
+            $MenuItems->label    = $col[0];
+            if(!empty($col[3]))
+            {$MenuItems->depth    = $col[3];}
             else
             {$MenuItems->depth    = 0;}
-            $cate_code = Category::where("ma" , $row[4])->first();
+            $cate_code = Category::where("ma" , $col[4])->first();
             if(!empty($cate_code)){
               $MenuItems->link           = $cate_code->slug;
               $MenuItems->category_code  = $cate_code->ma;
               $MenuItems->category_id    = $cate_code->id;
             }
            
-            if($row[2] !== ""){
-              $code  = $row[2];
+            if($col[2] !== ""){
+              $code  = $col[2];
               $cate  = MenuItems::where("ma" , $code)->first();
               if(!empty($cate)){
                 $MenuItems->parent = $cate->id;
               }
             }
 
-            if($row[5] == 1){
+            if($col[5] == 1){
                 $MenuItems->filter_by = 1;
-                $MenuItems->filter_value=$row[6];
+                $MenuItems->filter_value=$col[6];
               }
-              elseif ($row[5] == 2) {
+              elseif ($col[5] == 2) {
                 $MenuItems->filter_by = 2;
-                $MenuItems->filter_value=$row[6];
+                $MenuItems->filter_value=$col[6];
               }
-              elseif ($row[5] == 3) {
+              elseif ($col[5] == 3) {
                 $MenuItems->filter_by = 3;
               }
               else{
                 $MenuItems->filter_by = 0;
               }
+              if(!empty($col[7])){
+                $MenuItems->sort = $col[7];
+              }
+              else {
+                $MenuItems->sort = 0;
+              }
                 $MenuItems->save();
             }
-        else {
+        else{
+
             $MenuItems = new MenuItems();
-            $MenuItems->ma       = $row[1];
+            $MenuItems->label    = $col[0];
+            $MenuItems->ma       = $col[1];
             $MenuItems->menu     = $this->menu;
-            $MenuItems->label    = $row[0];
-            if(!empty($row[3]))
+
+            if(!empty($col[3]))
             {
-              $MenuItems->depth    = $row[3];
+              $MenuItems->depth    = $col[3];
             }
             else
             { 
               $MenuItems->depth    = 0;
             }
-            $cate_code = Category::where("ma" , $row[4])->first();
-            if(!empty($cate_code)){
-              $MenuItems->link           = $cate_code->slug;
-              $MenuItems->category_code  = $cate_code->ma;
-              $MenuItems->category_id    = $cate_code->id;
-            }
-            
-            if($row[2] !== ""){
-              $code  = $row[2];
-              $cate  = MenuItems::where("ma" , $code)->first();
-              if(!empty($cate)){
-                $MenuItems->parent = $cate->id;
+            // $cate_code = Category::where("ma" , $col[4])->first();
+            // if(!empty($cate_code)){
+            //   $MenuItems->link           = $cate_code->slug;
+            //   $MenuItems->category_code  = $cate_code->ma;
+            //   $MenuItems->category_id    = $cate_code->id;
+            // }
+              $cha ="";
+            if($col[2] !== ""){
+              $code  = $col[2];
+              $cha = MenuItems::where('ma',$code)->first();
+              if(!empty($cha)){
+                $MenuItems->parent = $cha->id;
               }
+              // else{
+              //   $MenuItems->parent = null;
+              // }
             }
 
-            if($row[5] == 1){
-                $MenuItems->filter_by = 1;
-                $MenuItems->filter_value=$row[6];
-              }
-              elseif ($row[5] == 2) {
-                $MenuItems->filter_by = 2;
-                $MenuItems->filter_value=$row[6];
-              }
-              elseif ($row[5] == 3) {
-                $MenuItems->filter_by = 3;
-              }
-              else{
-                $MenuItems->filter_by = 0;
-              }
-              if(!empty($row[7])){
-                $MenuItems->sort = $row[7];
+              // if($col[5] == 1){
+              //   $MenuItems->filter_by = 1;
+              //   $MenuItems->filter_value=$col[6];
+              // }
+              // elseif ($col[5] == 2) {
+              //   $MenuItems->filter_by = 2;
+              //   $MenuItems->filter_value=$col[6];
+              // }
+              // elseif ($col[5] == 3) {
+              //   $MenuItems->filter_by = 3;
+              // }
+              // else{
+              //   $MenuItems->filter_by = 0;
+              // }
+              if($col[7]!= ""){
+                $MenuItems->sort = $col[7];
               }
               else {
                 $MenuItems->sort = 0;
@@ -125,6 +136,11 @@ class MenuImport implements ToCollection, SkipsEmptyRows, WithStartRow, WithVali
               if($MenuItems->link ==null){
                 $MenuItems->link = "#";
               }
+
+              // if($col[7] = '51'){
+              //   dd($col,$MenuItems, $cha, $col[2] );
+              // }
+              
               $MenuItems->save();
             }
         }
