c9f0de3f72f2c5094c205d82a379b53ff4381b01
toanbv <sky97tb@gmail.com>
2022-10-26 17:59:26 +0700

    no message

--- resources/views/frontend/subsidebar.blade.php
+++ resources/views/frontend/subsidebar.blade.php
@@ -3,13 +3,30 @@
     @foreach($childs as $child)
         @if($menu == $child->menu)
         <li>
-            <a href="{!! $child->link !!}">{{$child->label}}</a>
-            @if(count($child->childs))
-                <span class="next-right"><i class="far fa-angle-right"></i></span>
-            @endif
-            </a>
-            @if(count($child->childs))
-                @include('frontend.subsidebar',[ 'childs' => $child->childs, 'menu' => $menu])
+            @if(!empty($child->link))
+                  <a href="https://{{$child->link}}">{{$child->label}}</a>
+            @else
+                @if(!empty($child->slug))
+                    <a href="{!!route('product_cat',['slug' => $child->slug,$child->filter_name => $child->filter_value])!!}">{{$child->label}}</a>
+                    @if(count($child->childs))
+                        <span class="next-right"><i class="far fa-angle-right"></i></span>
+                    @endif
+                    </a>
+                    @if(count($child->childs))
+                        @include('frontend.subsidebar',[ 'childs' => $child->childs, 'menu' => $menu])
+                    @endif
+
+                @else
+                    <a href="#">{{$child->label}}</a>
+                    @if(count($child->childs))
+                        <span class="next-right"><i class="far fa-angle-right"></i></span>
+                    @endif
+                    </a>
+                    @if(count($child->childs))
+                        @include('frontend.subsidebar',[ 'childs' => $child->childs, 'menu' => $menu])
+                    @endif
+
+                @endif
             @endif
         </li>
         @endif
--- resources/views/frontend/subsidebarmenu.blade.php
+++ resources/views/frontend/subsidebarmenu.blade.php
@@ -3,9 +3,31 @@
     <ul style="margin-left: 15px;">
         @foreach($childs as $child)
         @if($menu == $child->menu)
-        <li><a style="position:initial" href="{!!  $child->link !!}">{{$child->label}}</a>  
-            @if(count($child->childs))
-                @include('frontend.subsidebarmenu',['childs' => $child->childs,'menu' => $menu])
+        <li>
+            @if(!empty($child->link))
+                  <a style="position:initial"  href="https://{{$child->link}}">{{$child->label}}</a>
+            @else
+                @if(!empty($child->slug))
+                    <a style="position:initial"  href="{!!route('product_cat',['slug' => $child->slug,$child->filter_name => $child->filter_value])!!}">{{$child->label}}</a>
+                    @if(count($child->childs))
+                        <span class="next-right"><i class="far fa-angle-right"></i></span>
+                    @endif
+                    </a>
+                    @if(count($child->childs))
+                        @include('frontend.subsidebarmenu',[ 'childs' => $child->childs, 'menu' => $menu])
+                    @endif
+
+                @else
+                    <a style="position:initial"  href="#">{{$child->label}}</a>
+                    @if(count($child->childs))
+                        <span class="next-right"><i class="far fa-angle-right"></i></span>
+                    @endif
+                    </a>
+                    @if(count($child->childs))
+                        @include('frontend.subsidebarmenu',[ 'childs' => $child->childs, 'menu' => $menu])
+                    @endif
+
+                @endif
             @endif
         </li>
         @endif
